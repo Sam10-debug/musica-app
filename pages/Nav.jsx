a10c12d14a130aa73a03bc7fb782cc58ecda71d1
Emorinken <emorinkensam@gmail.com>
2022-10-19 03:50:48 -0700

    gave the hamburger a margin

--- pages/Nav.jsx
+++ pages/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
 	<header className="flex sticky top-0 justify-between md:justify-start md:gap-x-8 items-center p-4">
 		<nav className="flex gap-x-2">
-		<svg width="24" height="24" onClick={showNav} className='md:hidden z-auto' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<svg width="24" height="24" onClick={showNav} className='md:hidden z-auto mr-2' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<g clip-path="url(#clip0_107_495)">
 		<path d="M4 8H20" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 		<path d="M4 16H20" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
